f2381f1040f70f7e5e755b897fe305584d834ce6
kungreat <mydreams2018@outlook.com>
2022-12-17 18:48:44 +0800

    合作公司查询 图片上传

--- src/main/java/cn/kungreat/singlebbs/controller/manager/ManagerController.java
+++ src/main/java/cn/kungreat/singlebbs/controller/manager/ManagerController.java
@@ -9,11 +9,17 @@ import cn.kungreat.singlebbs.service.ManagerService;
 import cn.kungreat.singlebbs.service.UserService;
 import cn.kungreat.singlebbs.vo.JsonResult;
 import cn.kungreat.singlebbs.vo.QueryResult;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
@@ -25,6 +31,10 @@ public class ManagerController {
     private UserService userService;
     @Autowired
     private ManagerService managerService;
+    @Value("${user.imgPath}")
+    private String path;
+    @Value("#{'${user.manager}'.split(',')}")
+    private List<String> manager;
 
     @RequestMapping(value = "/getAllUser", method = RequestMethod.POST)
     public QueryResult getAllUser(UserQuery userQuery) {
@@ -88,4 +98,28 @@ public class ManagerController {
     public Map<String, Integer> selectAuthCount() {
         return managerService.selectAuthCount();
     }
+    @RequestMapping(value = "/uploadImg")
+    public JsonResult uploadImg(MultipartFile file){
+        JsonResult jsonResult = new JsonResult();
+        try{
+            String account = SecurityContextHolder.getContext().getAuthentication().getName();
+            Assert.isTrue(manager.contains(account),"没有权限操作此接口");
+            Assert.isTrue("image/jpeg".equals(file.getContentType())
+                    ||"image/gif".equals(file.getContentType())
+                    || "image/jpg".equals(file.getContentType()),"只支持jpg或gif格式的图片");
+            String type = file.getContentType().split("/")[1];
+            String name = RandomStringUtils.randomAlphabetic(8);
+            String rans = name +"."+type;
+            String img = path +"userImg/collaboration/"+rans;
+            file.transferTo(new File(img));
+            String path = "/api/userImg/collaboration/"+rans;
+            jsonResult.setAction(path);
+        }catch (Exception e){
+            jsonResult.setResult(false);
+            jsonResult.setStatus(0);
+            jsonResult.setMsg(e.getMessage());
+        }
+        return jsonResult;
+    }
+
 }
